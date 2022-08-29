ede4a417f5df2057098895c7826b9b061db808e2
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2022-08-29 11:25:18 -0300

    removido rotas não utilizadas

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HojeController;
-use App\Http\Controllers\TabController;
-use App\Http\Controllers\PiramideController;
-use App\Http\Controllers\ConeController;
 use App\Http\Controllers\AgendaController;
 
 /*
@@ -21,21 +17,5 @@ use App\Http\Controllers\AgendaController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/jogadores', function() {
-    echo "1 - Neymar<br>";
-    echo "2 - Pelé<br>";
-    echo "3 - Zico<br>";
-});
-
-Route::redirect('/players', '/jogadores');
-
-Route::get('/hoje', [HojeController::class, 'teste']);
-
-Route::get('/tab/{id?}/{ini?}/{fim?}', [TabController::class, 'tab']);
-
-Route::get('/piramide/{ab?}/{h?}/{tt?}', [PiramideController::class, 'index'])->where(['ab' => '[0-9999.99]+', 'h' => '[0-9999.99]+', 'tt' => '[1-3]']);
-
-Route::get('/cone/{r?}/{h?}/{tt?}', [ConeController::class, 'index'])->where(['r' => '[0-9999.99]+', 'h' => '[0-9999.99]+', 'tt' => '[1-3]']);
 
 Route::resource('/agenda', AgendaController::class);
